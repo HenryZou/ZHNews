9a00b611b26925755ba7bc3dec9564398773b985
HenryZou <136845218@qq.com>
2016-02-26 23:08:50 +0800

    first

---
+++ ZHNews/ZHNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHNews
+//
+//  Created by Henry on 16/2/26.
+//  Copyright © 2016年 Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZHNews/ZHNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZHNews
+//
+//  Created by Henry on 16/2/26.
+//  Copyright © 2016年 Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
